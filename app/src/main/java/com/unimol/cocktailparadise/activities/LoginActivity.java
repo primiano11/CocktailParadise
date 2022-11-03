c1326fbd3e1cd4eb83c7bc4b65650c34c7831098
Primiano <primiano.trivisonno@gmail.com>
2022-11-03 21:32:32 +0100

    Inizializzazione progetto

---
+++ app/src/main/java/com/unimol/cocktailparadise/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.unimol.cocktailparadise.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.unimol.cocktailparadise.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/unimol/cocktailparadise/activities/MainActivity.java
@@ -0,0 +1,58 @@
+package com.unimol.cocktailparadise.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.unimol.cocktailparadise.R;
+import com.unimol.cocktailparadise.ui.home.HomeFragment;
+import com.unimol.cocktailparadise.ui.profile.ProfileFragment;
+import com.unimol.cocktailparadise.ui.search.SearchFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                        return true;
+                }
+
+                return false;
+
+            }
+        });
+
+
+
+    }
+}
