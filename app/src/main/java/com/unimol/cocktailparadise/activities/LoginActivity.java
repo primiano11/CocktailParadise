5329912428a65e49f091415a4767e4414dfe84d0
Primiano <primiano.trivisonno@gmail.com>
2022-11-04 00:34:37 +0100

    .

--- app/src/main/java/com/unimol/cocktailparadise/activities/LoginActivity.java
+++ app/src/main/java/com/unimol/cocktailparadise/activities/LoginActivity.java
@@ -3,11 +3,24 @@ package com.unimol.cocktailparadise.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.unimol.cocktailparadise.R;
+import com.unimol.cocktailparadise.models.LoginDTO;
+import com.unimol.cocktailparadise.network.RetrofitClient;
+import com.unimol.cocktailparadise.network.UserService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,7 +39,35 @@ public class LoginActivity extends AppCompatActivity {
         this.passwordBox = (EditText) findViewById(R.id.passwordBox);
         this.loginButton = (Button) findViewById(R.id.loginButton);
         this.registerLink = (TextView) findViewById(R.id.registerLink);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                UserService userService = RetrofitClient.getRetrofitInstanceMYSQ().create(UserService.class);
+                Call<LoginDTO> call = userService.login(mailBox.toString(), passwordBox.toString());
+
+
+                call.enqueue(new Callback<LoginDTO>() {
+                    @Override
+                    public void onResponse(Call<LoginDTO> call, Response<LoginDTO> response) {
+                        Log.e("CIAO", "CIAO");
+                        LoginDTO loginDTO = response.body();
+
+                        if(loginDTO.getStatus() == true){
+                            Log.e("RESULT", "LOGIN OK" );
+                        } else {
+                            Log.e("RESULT", "LOGIN FALLITO");
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginDTO> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
     }
-
-
 }
--- app/src/main/java/com/unimol/cocktailparadise/network/UserService.java
+++ app/src/main/java/com/unimol/cocktailparadise/network/UserService.java
@@ -4,10 +4,11 @@ import com.unimol.cocktailparadise.models.LoginDTO;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface UserService {
 
-    @GET
-    Call<LoginDTO> login(String mail, String password);
+    @GET("user/login")
+    Call<LoginDTO> login(@Query("mail") String mail, @Query("password") String password);
 
 }
